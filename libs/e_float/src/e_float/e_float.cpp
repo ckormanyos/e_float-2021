fb6df53d8d00cfe7de6c7316b4c508b55e5ee9fb
Christopher Kormanyos <ckormanyos@yahoo.com>
2021-02-09 23:29:50 +0100

    Retry again linker fix

--- libs/e_float/src/e_float/e_float.cpp
+++ libs/e_float/src/e_float/e_float.cpp
@@ -27,28 +27,26 @@ const e_float& ef::zero() { static const e_float val(0U);    return val; }
 const e_float& ef::one () { static const e_float val(1U);    return val; }
 const e_float& ef::half() { static const e_float val("0.5"); return val; }
 
-#if defined(__GNUC__) || defined(__clang__)
-constexpr bool                    numeric_limits<e_float>::is_specialized;
-constexpr bool                    numeric_limits<e_float>::is_signed;
-constexpr bool                    numeric_limits<e_float>::is_integer;
-constexpr bool                    numeric_limits<e_float>::is_exact;
-constexpr bool                    numeric_limits<e_float>::is_bounded;
-constexpr bool                    numeric_limits<e_float>::is_modulo;
-constexpr bool                    numeric_limits<e_float>::is_iec559;
-constexpr std::int64_t            numeric_limits<e_float>::digits;
-constexpr std::int64_t            numeric_limits<e_float>::digits10;
-constexpr std::int64_t            numeric_limits<e_float>::max_digits10;
-constexpr std::int64_t            numeric_limits<e_float>::min_exponent;
-constexpr std::int64_t            numeric_limits<e_float>::min_exponent10;
-constexpr std::int64_t            numeric_limits<e_float>::max_exponent;
-constexpr std::int64_t            numeric_limits<e_float>::max_exponent10;
-constexpr int                     numeric_limits<e_float>::radix;
-constexpr std::float_round_style  numeric_limits<e_float>::round_style;
-constexpr bool                    numeric_limits<e_float>::has_infinity;
-constexpr bool                    numeric_limits<e_float>::has_quiet_NaN;
-constexpr bool                    numeric_limits<e_float>::has_signaling_NaN;
-constexpr std::float_denorm_style numeric_limits<e_float>::has_denorm;
-constexpr bool                    numeric_limits<e_float>::has_denorm_loss;
-constexpr bool                    numeric_limits<e_float>::traps;
-constexpr bool                    numeric_limits<e_float>::tinyness_before;
-#endif
+constexpr bool                    std::numeric_limits<e_float>::is_specialized;
+constexpr bool                    std::numeric_limits<e_float>::is_signed;
+constexpr bool                    std::numeric_limits<e_float>::is_integer;
+constexpr bool                    std::numeric_limits<e_float>::is_exact;
+constexpr bool                    std::numeric_limits<e_float>::is_bounded;
+constexpr bool                    std::numeric_limits<e_float>::is_modulo;
+constexpr bool                    std::numeric_limits<e_float>::is_iec559;
+constexpr std::int64_t            std::numeric_limits<e_float>::digits;
+constexpr std::int64_t            std::numeric_limits<e_float>::digits10;
+constexpr std::int64_t            std::numeric_limits<e_float>::max_digits10;
+constexpr std::int64_t            std::numeric_limits<e_float>::min_exponent;
+constexpr std::int64_t            std::numeric_limits<e_float>::min_exponent10;
+constexpr std::int64_t            std::numeric_limits<e_float>::max_exponent;
+constexpr std::int64_t            std::numeric_limits<e_float>::max_exponent10;
+constexpr int                     std::numeric_limits<e_float>::radix;
+constexpr std::float_round_style  std::numeric_limits<e_float>::round_style;
+constexpr bool                    std::numeric_limits<e_float>::has_infinity;
+constexpr bool                    std::numeric_limits<e_float>::has_quiet_NaN;
+constexpr bool                    std::numeric_limits<e_float>::has_signaling_NaN;
+constexpr std::float_denorm_style std::numeric_limits<e_float>::has_denorm;
+constexpr bool                    std::numeric_limits<e_float>::has_denorm_loss;
+constexpr bool                    std::numeric_limits<e_float>::traps;
+constexpr bool                    std::numeric_limits<e_float>::tinyness_before;
